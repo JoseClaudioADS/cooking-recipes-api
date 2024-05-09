51bec313d4cbed88c9851be77ffa420f322b4fb5
Jose Claudio <jcsj.ti@gmail.com>
2024-05-09 15:37:46 -0300

    add first setup e2e tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -9,7 +9,11 @@ export default defineConfig({
     test: {
         clearMocks: true,
         globals: true,
-        setupFiles: ["dotenv/config"]
+        setupFiles: ["dotenv/config"],
+        env: {
+            NODE_ENV: "test",
+            DATABASE_URL: "postgresql://cooking-recipes-user:cooking-recipes-password@localhost:5432/cooking-recipes-test?schema=public"
+        }
     },
     resolve: {
         alias: [{ find: "~", replacement: resolve(__dirname, "src") }]
