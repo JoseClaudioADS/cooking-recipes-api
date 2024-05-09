0578282dbf5b837f93646782d4cbf1c2f57c0e2d
Jose Claudio <jcsj.ti@gmail.com>
2024-05-09 10:39:27 -0300

    fix create photo tests

--- src/core/photos/use-case/create-photo.use-case.test.ts
+++ src/core/photos/use-case/create-photo.use-case.test.ts
@@ -1,6 +1,8 @@
 import { faker } from "@faker-js/faker";
 import { beforeAll, beforeEach, describe, expect, it, vi } from "vitest";
 import { ZodError } from "zod";
+import { AuthUser } from "../../shared/entity/auth-user";
+import { UploadService } from "../../shared/services/upload.service";
 import { PhotosRepository } from "../repository/photos.repository";
 import { CreatePhotoInput, CreatePhotoUseCase } from "./create-photo.use-case";
 
@@ -9,13 +11,24 @@ describe("CreatePhotoUseCase", () => {
 
     let useCase: CreatePhotoUseCase;
     let photosRepository: PhotosRepository;
+    let uploadService: UploadService;
+
+    const authUser: AuthUser = {
+        id: faker.number.int(),
+        name: faker.name.firstName(),
+        email: faker.internet.email()
+    };
 
     beforeAll(() => {
         photosRepository = {
             create: vi.fn()
         } as unknown as PhotosRepository;
 
-        useCase = new CreatePhotoUseCase(photosRepository);
+        uploadService = {
+            upload: vi.fn()
+        } as unknown as UploadService;
+
+        useCase = new CreatePhotoUseCase(photosRepository, uploadService);
     });
 
     beforeEach(() => {
@@ -25,7 +38,8 @@ describe("CreatePhotoUseCase", () => {
     describe("given a valid input", () => {
 
         const input: CreatePhotoInput = {
-            filename: faker.lorem.word()
+            filename: faker.lorem.word(),
+            data: Buffer.from([])
         };
 
         it("should create a photo", async () => {
@@ -34,23 +48,24 @@ describe("CreatePhotoUseCase", () => {
 
             vi.spyOn(photosRepository, "create").mockResolvedValueOnce(repositoryOutput);
 
-            const result = await useCase.execute(input);
+            const result = await useCase.execute(input, authUser);
 
             expect(result).toStrictEqual({
                 id: repositoryOutput.id
             });
-            expect(photosRepository.create).toHaveBeenCalledWith({ filename: input.filename });
+            expect(photosRepository.create).toHaveBeenCalledWith({ filename: expect.any(String) });
         });
     });
 
     describe("given an invalid input", () => {
 
         const input: CreatePhotoInput = {
-            filename: ""
+            filename: "",
+            data: 1 as unknown as Buffer
         };
 
         it("should not create a photo", async () => {
-            await expect(useCase.execute(input)).rejects.toThrow(ZodError);
+            await expect(useCase.execute(input, authUser)).rejects.toThrow(ZodError);
 
             expect(photosRepository.create).not.toHaveBeenCalled();
         });
